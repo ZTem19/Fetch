09ec27e6431a4c323ca732e1a6b4c7876a69a905
ttagh <thessa.taghon@ndsu.edu>
2024-12-18 20:36:42 -0600

    added new pictures in assets

--- Fetch/obj/Debug/net8.0/FetchApp.AssemblyInfo.cs
+++ Fetch/obj/Debug/net8.0/FetchApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FetchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b768aeb9ab555122baad57732fe9edf69c4c971c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30f453befb2b7a9c740042a57cdec0abeec0b90")]
 [assembly: System.Reflection.AssemblyProductAttribute("FetchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FetchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
